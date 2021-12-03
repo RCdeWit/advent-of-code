ca9bf8e498df43dd3f419e2bdf0530dc88143355
Rob de Wit <git@rcdewit.nl>
2021-12-03 14:09:14 +0100

    Add solutions for day 3

---
+++ day3/implementation-1.py
@@ -0,0 +1,36 @@
+def calculate_rate(method, input):
+    output = [0] * len(input[0])
+
+    for row, val in enumerate(input):
+        for col, bit in enumerate(val):
+            if bit == "1":
+                output[col] += 1
+            else:
+                output[col] -= 1
+
+    if method == "epsilon":
+        for col, val in enumerate(output):
+            output[col] = val * -1
+
+    for col, val in enumerate(output):
+        if val < 0:
+            output[col] = 0
+        if val > 0:
+            output[col] = 1
+
+    # Convert to string again
+    output_string = ""
+    for col in output:
+        output_string += str(col)
+
+    return output_string
+
+
+with open('input.txt') as f:
+    diagnostics = list(f.read().splitlines())
+
+# Convert binary to decimal
+epsilon = int(calculate_rate("epsilon", diagnostics), 2)
+gamma = int(calculate_rate("gamma", diagnostics), 2)
+
+print(epsilon, gamma, epsilon * gamma)
---
+++ day3/implementation-2.py
@@ -0,0 +1,40 @@
+def get_most_common_value_in_column(input, column):
+    output = [0] * len(input[0])
+
+    for row, val in enumerate(input):
+        for col, bit in enumerate(val):
+            if bit == "1":
+                output[col] += 1
+            else:
+                output[col] -= 1
+
+    if output[column] >= 0:
+        return 1
+    else:
+        return 0
+
+def reduce_list_by_common_columns(input, metric):
+    output = input
+
+    for i, val in enumerate(output[0]):
+        most_common_value = get_most_common_value_in_column(output, i)
+        temp_list = []
+        for j, val2 in enumerate(output):
+
+            if int(output[j][i]) == most_common_value and metric == "most":
+                temp_list.append(output[j])
+            elif int(output[j][i]) != most_common_value and metric == "least":
+                temp_list.append(output[j])
+
+        output = temp_list
+
+        if len(output) == 1:
+            return(output)[0]
+
+with open('input.txt') as f:
+    diagnostics = list(f.read().splitlines())
+
+oxygen = int(reduce_list_by_common_columns(diagnostics, "most"), 2)
+scrubber = int(reduce_list_by_common_columns(diagnostics, "least"), 2)
+
+print(oxygen, scrubber, oxygen * scrubber)
