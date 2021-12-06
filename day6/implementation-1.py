b824f2feec29884c9cf3e71ef5a4c6904d280670
Rob de Wit <git@rcdewit.nl>
2021-12-06 11:29:27 +0100

    Add solutions for day 6

---
+++ day6/implementation-1.py
@@ -0,0 +1,20 @@
+with open('input.txt') as f:
+    list_of_fish = list(map(int, f.read().split(',')))
+
+# Remove trailing newline
+# list_of_fish = list_of_fish[:-1]
+
+n_days = 80
+for i in range(n_days):
+    temp_list_of_fish = []
+    for f, val in enumerate(list_of_fish):
+        if val > 0:
+            temp_list_of_fish.append(val - 1)
+        else:
+            temp_list_of_fish.append(6)
+            temp_list_of_fish.append(8)
+
+    list_of_fish = temp_list_of_fish
+    # print(f"Day {i}, {len(list_of_fish)} fish: {list_of_fish}")
+
+print(len(list_of_fish))
---
+++ day6/implementation-2.py
@@ -0,0 +1,22 @@
+with open('input.txt') as f:
+    list_of_fish = list(map(int, f.read().split(',')))
+
+n_days = 256
+
+# Aggregate fish by day rather than simulate them all individually
+summed_list_of_fish = [0] * 9
+for f, val in enumerate(list_of_fish):
+    summed_list_of_fish[val] += 1
+
+# Every day find the number of fish who are fish_reproducing
+# Reset those fish to a week
+# Add the new fish to the end of the array
+# And remove today from the array
+for i in range(n_days):
+    fish_reproducing = summed_list_of_fish[0]
+
+    summed_list_of_fish[7] += fish_reproducing
+    summed_list_of_fish.append(fish_reproducing)
+    summed_list_of_fish = summed_list_of_fish[1:]
+
+print(sum(summed_list_of_fish))
