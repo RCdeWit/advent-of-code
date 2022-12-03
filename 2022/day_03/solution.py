93bb9f56a64fed79fc8573fc96b9c0dd3d8d6ad4
RCdeWit <RCdeWit@users.noreply.github.com>
2022-12-04 00:09:30 +0100

    Refactor and provide solution for part 2

--- 2022/day_03/solution.py
+++ 2022/day_03/solution.py
@@ -26,25 +26,25 @@ def parse_letter_to_value(letter):
     else:
         print(f"Invalid letter: {letter}")
 
-# Take a rugsack, parse values, and split into compartments
-def split_into_compartments(rugsack):
+def parse_rugsack(rugsack):
     rugsack_parsed = []
 
     for i in rugsack:
         rugsack_parsed.append(parse_letter_to_value(i))
 
+    return rugsack_parsed
+
+# Take a rugsack, parse values, and split into compartments
+def split_into_compartments(rugsack):
     full = len(rugsack)
     half = int(full / 2)
 
-    compartment_1 = rugsack_parsed[0:half]
-    compartment_2 = rugsack_parsed[half:full]
+    compartment_1 = rugsack[0:half]
+    compartment_2 = rugsack[half:full]
 
     return([compartment_1, compartment_2])
 
-def find_intersection_compartments(rugsack):
-    compartment_1 = rugsack[0]
-    compartment_2 = rugsack[1]
-
+def find_intersection_compartments(compartment_1, compartment_2):
     intersection = set(compartment_1).intersection(compartment_2)
     return intersection
 
@@ -52,19 +52,35 @@ def find_intersection_compartments(rugsack):
 # Print results depending on the question (1 or 2)
 match question:
     case "1":
-        rugsack = split_into_compartments(input[0])
-
         result = 0
 
         for rs in input:
-            rugsack = split_into_compartments(rs)
-            intersection = find_intersection_compartments(rugsack)
+            rugsack = parse_rugsack(rs)
+            rugsack = split_into_compartments(rugsack)
+            intersection = find_intersection_compartments(rugsack[0], rugsack[1])
 
             result = result + max(intersection)
         
         print(result)
 
-
-
     case "2":
-        print(0)
+        result = 0
+        rugsack_list = []
+
+        for ln in input:
+            rugsack_list.append(parse_rugsack(ln))
+
+        # Treat lines in blocks of three
+        i = 0
+        while i < len(rugsack_list):
+            # First find intersection between 1 and 2
+            intersection = find_intersection_compartments(rugsack_list[i], rugsack_list[i+1])
+            # Then intersection with previous result and 3
+            intersection = find_intersection_compartments(intersection, rugsack_list[i+2])
+
+            result = result + max(intersection)
+
+            # Jump to next block
+            i = i + 3
+
+        print(result)
