a7a22340a57222bee6769455d4afc98e6e42f4a2
RCdeWit <RCdeWit@users.noreply.github.com>
2022-12-04 13:34:52 +0100

    Implementation for Q1

--- 2022/day_04/solution.py
+++ 2022/day_04/solution.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 
 # Parse CLI arguments
 parser = argparse.ArgumentParser()
@@ -13,74 +14,46 @@ question = args.question
 with open(input_file) as f:
     input = list(f.read().splitlines())
 
-# Change a letter to a value (a-z: 1-26; A-Z:27-52)
-def parse_letter_to_value(letter):
-    ascii = ord(letter)
+    # Capture numbers as four groups
+    regex = "^(\d+)-(\d+),(\d+)-(\d+)$"
 
-    if 65 <= ascii <= 90:
-        # Upper case
-        return ascii - 38
-    elif 97 <= ascii <= 122:
-        # Lower case
-        return ascii - 96
+    input_parsed = []
+
+    # Extract number groups from each line, add to list
+    for l in input:
+        parsed = re.search(regex, l).groups()
+
+        elf_1 = [int(parsed[0]), int(parsed[1])]
+        elf_2 = [int(parsed[2]), int(parsed[3])]
+
+        input_parsed.append([elf_1, elf_2])
+
+    input = input_parsed
+
+def section_fully_contains_other_section(section_1, section_2):
+    start_1 = section_1[0]
+    end_1 = section_1[1]
+    start_2 = section_2[0]
+    end_2 = section_2[1]
+
+    if start_1 <= start_2 and end_1 >= end_2:
+        return True
+    elif start_2 <= start_1 and end_2 >= end_1:
+        return True
     else:
-        print(f"Invalid letter: {letter}")
+        return False
 
-def parse_rugsack(rugsack):
-    rugsack_parsed = []
-
-    for i in rugsack:
-        rugsack_parsed.append(parse_letter_to_value(i))
-
-    return rugsack_parsed
-
-# Take a rugsack, parse values, and split into compartments
-def split_into_compartments(rugsack):
-    full = len(rugsack)
-    half = int(full / 2)
-
-    compartment_1 = rugsack[0:half]
-    compartment_2 = rugsack[half:full]
-
-    return([compartment_1, compartment_2])
-
-def find_intersection_compartments(compartment_1, compartment_2):
-    intersection = set(compartment_1).intersection(compartment_2)
-    return intersection
-
+        
 
 # Print results depending on the question (1 or 2)
 match question:
     case "1":
-        result = 0
+        count = 0
 
-        for rs in input:
-            rugsack = parse_rugsack(rs)
-            rugsack = split_into_compartments(rugsack)
-            intersection = find_intersection_compartments(rugsack[0], rugsack[1])
+        for pair in input:
+            count = count + section_fully_contains_other_section(pair[0], pair[1])
 
-            result = result + max(intersection)
-        
-        print(result)
+        print(count)
 
     case "2":
-        result = 0
-        rugsack_list = []
-
-        for ln in input:
-            rugsack_list.append(parse_rugsack(ln))
-
-        # Treat lines in blocks of three
-        i = 0
-        while i < len(rugsack_list):
-            # First find intersection between 1 and 2
-            intersection = find_intersection_compartments(rugsack_list[i], rugsack_list[i+1])
-            # Then intersection with previous result and 3
-            intersection = find_intersection_compartments(intersection, rugsack_list[i+2])
-
-            result = result + max(intersection)
-
-            # Jump to next block
-            i = i + 3
-
-        print(result)
+        print(0)
