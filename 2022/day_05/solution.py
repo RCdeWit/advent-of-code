9e71f66cd526396130c282b6a73aaa14c7c518b5
RCdeWit <RCdeWit@users.noreply.github.com>
2022-12-05 11:33:26 +0100

    Boilerplate day 5

---
+++ 2022/day_05/solution.py
@@ -0,0 +1,24 @@
+import argparse
+import re
+
+# Parse CLI arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", required=True)
+parser.add_argument("-q", "--question", required=True)
+args = parser.parse_args()
+
+input_file = args.input
+question = args.question
+
+# Read input
+with open(input_file) as f:
+    input = list(f.read().splitlines())
+
+
+# Print results depending on the question (1 or 2)
+match question:
+    case "1":
+        print(0)
+
+    case "2":
+        print(0)
