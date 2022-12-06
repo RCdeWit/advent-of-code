93f878993a34de45719ce60381c61e2c44ec9206
RCdeWit <RCdeWit@users.noreply.github.com>
2022-12-06 12:25:47 +0100

    Solution part 1

--- 2022/day_06/solution.py
+++ 2022/day_06/solution.py
@@ -12,12 +12,27 @@ question = args.question
 
 # Read input
 with open(input_file) as f:
-    input = list(f.read().splitlines())
-        
+    input = list(f.read())
+
+def find_start_of_packet(datastream):
+    buffer = []
+
+    for i, c in enumerate(datastream):
+        buffer.append(c)
+
+        if len(buffer) == 4:
+            # Set retains unique values in list
+            # If its length < 4, there's a duplicate in the buffer
+            # Otherwise we have found the start of the packet
+            if len(set(buffer)) == 4:
+                return(i+1)
+
+            buffer.pop(0)
+
 # Print results depending on the question (1 or 2)
 match question:
     case "1":
-        print(1)
-
+        result = find_start_of_packet(input)
+        print(result)
     case "2":
         print(2)
