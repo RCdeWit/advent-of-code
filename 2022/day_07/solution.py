4637d3408047f33472357fce2bfe62f280b9a586
RCdeWit <RCdeWit@users.noreply.github.com>
2022-12-07 18:37:23 +0100

    Working for example input

--- 2022/day_07/solution.py
+++ 2022/day_07/solution.py
@@ -1,5 +1,5 @@
 import argparse
-import re
+import sys
 
 # Parse CLI arguments
 parser = argparse.ArgumentParser()
@@ -12,11 +12,65 @@ question = args.question
 
 # Read input
 with open(input_file) as f:
-    input = list(f.read())
+    input = list(f.read().splitlines())
+
+def parse_input_to_file_system(input):   
+    filesystem = {}
+    dirstack = []
+    current = filesystem
+
+    for current_line in input:
+        if current_line[0] == "$":
+            if current_line.startswith("$ cd .."):
+                dirstack.pop()
+
+                current = filesystem
+                for entry in dirstack:
+                    current = filesystem[entry]
+
+            elif current_line.startswith("$ cd"):
+                destination = current_line[5:]
+                if destination == "/":
+                    dirstack = []
+                    current = filesystem
+                else:
+                    dirstack.append(destination)
+                    current = current[destination]
+
+        elif current_line.startswith('dir '):
+            dir_name = current_line.split(' ')[1]
+            current[dir_name] = {}
+        else:
+            file_name = current_line.split(' ')[1]
+            file_size = current_line.split(' ')[0]
+
+            current[file_name] = file_size
+
+    return filesystem
+
+def find_totals(input):
+    total = 0
+
+    for key, value in input.items():
+        # File
+        if type(value) == str:
+            total = total + int(value)
+        # Directory
+        else:
+            total = total + find_totals(value)
+
+            if total <= 100000:
+                result.append(total)
+
+    return(total)
+
+result = []
 
 # Print results depending on the question (1 or 2)
 match question:
     case "1":
-        print(1)
+        file_system = parse_input_to_file_system(input)
+        print(find_totals(file_system))
+        print(sum(result))
     case "2":
-        print(2)
+        None
--- 2022/template/solution.py
+++ 2022/template/solution.py
@@ -14,6 +14,43 @@ question = args.question
 with open(input_file) as f:
     input = list(f.read())
 
+def parse_input(input):
+
+    file_structure = {}
+
+    total_lines = len(input)
+    current_line = 0
+
+    current_directory = ""
+    buffer_list = {}
+
+    while current_line <= total_lines:
+        # If command
+        if input[current_line][0] == "$":
+            # Write output from previous command to file_structure
+            # Clear file structure
+            # Execute command
+
+            if input[current_line][0:4] == "$ cd":
+                current_directory = input[current_line][5:]
+                print(current_directory)
+        
+        else:
+            if input[current_line][0:3] == "dir":
+                # Add dir to buffer list
+                new_level = {}
+                buffer_list.append(input[current_line])
+            else:
+                buffer_list.append(input[current_line])
+
+        
+        current_line = current_line + 1
+
+    return structure
+
+        
+
+
 # Print results depending on the question (1 or 2)
 match question:
     case "1":
