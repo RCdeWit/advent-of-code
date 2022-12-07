228e66427754fcae09300ffcd01fbb61394f6cab
RCdeWit <RCdeWit@users.noreply.github.com>
2022-12-08 00:27:43 +0100

    Clean up boilerplate file

--- 2022/template/solution.py
+++ 2022/template/solution.py
@@ -1,5 +1,4 @@
 import argparse
-import re
 
 # Parse CLI arguments
 parser = argparse.ArgumentParser()
@@ -13,43 +12,6 @@ question = args.question
 # Read input
 with open(input_file) as f:
     input = list(f.read())
-
-def parse_input(input):
-
-    file_structure = {}
-
-    total_lines = len(input)
-    current_line = 0
-
-    current_directory = ""
-    buffer_list = {}
-
-    while current_line <= total_lines:
-        # If command
-        if input[current_line][0] == "$":
-            # Write output from previous command to file_structure
-            # Clear file structure
-            # Execute command
-
-            if input[current_line][0:4] == "$ cd":
-                current_directory = input[current_line][5:]
-                print(current_directory)
-        
-        else:
-            if input[current_line][0:3] == "dir":
-                # Add dir to buffer list
-                new_level = {}
-                buffer_list.append(input[current_line])
-            else:
-                buffer_list.append(input[current_line])
-
-        
-        current_line = current_line + 1
-
-    return structure
-
-        
-
 
 # Print results depending on the question (1 or 2)
 match question:
