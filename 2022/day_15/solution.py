7d1a52cd3aee2b383004faf84d0c60191f49f6d4
RCdeWit <RCdeWit@users.noreply.github.com>
2022-12-16 10:39:26 +0100

    Naive solution Q1

--- 2022/day_15/solution.py
+++ 2022/day_15/solution.py
@@ -10,13 +10,17 @@ args = parser.parse_args()
 input_file = args.input
 question = args.question
 
+def get_radius(location_1, location_2):
+    return abs(location_1[0] - location_2[0]) + abs(location_1[1] - location_2[1])
+
 # Read input
 with open(input_file) as f:
     input = list(f.read().splitlines())
 
     regex = "^.+x=(-?\d+). y=(-?\d+).+x=(-?\d+), y=(-?\d+)$"
 
-    pairs = {}
+    sensors = {}
+    beacons = {}
 
     min_x = float('inf')
     max_x = -float('inf')
@@ -31,21 +35,23 @@ with open(input_file) as f:
         beacon_x = int(coordinates[2])
         beacon_y = int(coordinates[3])
 
-        if sensor_x > max_x:
-            max_x = sensor_x
+        radius = get_radius([sensor_x, sensor_y], [beacon_x, beacon_y])
+
+        if sensor_x + (radius) > max_x:
+            max_x = sensor_x + (radius)
         if beacon_x > max_x:
             max_x = beacon_x
-        if sensor_y > max_y:
-            max_y = sensor_y
+        if sensor_y + (radius) > max_y:
+            max_y = sensor_y + (radius)
         if beacon_y > max_y:
-            max_y = beacon_y
+            max_y = beacon_y 
 
-        if sensor_x < min_x:
-            min_x = sensor_x
+        if sensor_x - (radius) < min_x:
+            min_x = sensor_x - (radius)
         if beacon_x < min_x:
             min_x = beacon_x
-        if sensor_y < min_y:
-            min_y = sensor_y
+        if sensor_y - (radius) < min_y:
+            min_y = sensor_y - (radius)
         if beacon_y < min_y:
             min_y = beacon_y
 
@@ -57,70 +63,42 @@ with open(input_file) as f:
         beacon_x = int(coordinates[2])
         beacon_y = int(coordinates[3])
 
-        pairs[sensor_x-min_x, sensor_y-min_y] = [beacon_x-min_x, beacon_y-min_y]
+        sensors[sensor_x, sensor_y] = get_radius([sensor_x, sensor_y], [beacon_x, beacon_y])
+        beacons[beacon_x, beacon_y] = 1
 
-    max_x = max_x - min_x
-    max_y = max_y - min_y
-    min_x = 0
-    min_y = 0
+def is_in_radius(point, sensor, radius):
+    point_x = point[0]
+    point_y = point[1]
+    sensor_x = sensor[0]
+    sensor_y = sensor[1]
 
-    input = pairs
+    delta_x = abs(sensor_x - point_x)
+    delta_y = abs(sensor_y - point_y)
 
-def create_map(input, min_x, max_x, min_y, max_y):
-    map = [ [ "." for x in range( max_x + 1 ) ] for y in range( max_y + 1 ) ]
-
-    for i, key in enumerate(list(input.keys())):
-        sensor_x = key[0]
-        sensor_y = key[1]
-        beacon_x = input[key][0]
-        beacon_y = input[key][1]
-
-        map[sensor_y][sensor_x] = "S"
-        map[beacon_y][beacon_x] = "B"
-
-        delta_x = abs(sensor_x - beacon_x)
-        delta_y = abs(sensor_y - beacon_y)
-
-        delta_total = delta_x + delta_y
-        for dx in range(-delta_total, delta_total + 1):
-            for dy in range(-delta_total, delta_total + 1):
-
-                if abs(dx) + abs(dy) <= delta_total:
-                    # Out of bounds check
-                    if sensor_y+dy >= 0 and sensor_y+dy < len(map) and sensor_x+dx >= 0 and sensor_x+dx < len(map[0]):
-
-                        # Draw range
-                        if map[sensor_y+dy][sensor_x+dx] == ".":
-                            map[sensor_y+dy][sensor_x+dx] = "#"
-
-    return map
-        
-def draw_map(map):
-    for y, row in enumerate(map):
-        line = ""
-        count = 0
-        for character in row:
-            line = line + str(character)
-            if character == "#":
-                count = count + 1
-        print(str(y).zfill(2), line, "- Count:", count)
-
-def count_characters(array, line, character="#"):
-    result = 0
-    for c in array[line]:
-        if c == character:
-            result = result + 1
-
-    return result
+    return delta_x + delta_y <= radius
 
 
 # Print results depending on the question (1 or 2)
 match question:
     case "1":
-        map = create_map(input, min_x, max_x, min_y, max_y)
-        draw_map(map)
+        y = 2000000
+        covered = 0
+        for x in range(min_x, max_x):
+            print(x, "/", max_x)
+            for s in sensors:
+                in_radius = is_in_radius([x, y], s, sensors[s])
 
-        print(count_characters(map, 10, "#"))
+                if in_radius:
+                    covered = covered + 1
+                    break
+
+        beacons_on_line = 0
+        for b in beacons:
+            if b[1] == y:
+                beacons_on_line = beacons_on_line + 1
+
+        print(covered, beacons_on_line)
+        print(covered - beacons_on_line)
 
     case "2":
         print(2)
