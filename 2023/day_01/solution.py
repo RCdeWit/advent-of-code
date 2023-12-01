36c0b6d01f2a74e730ccb4fcb73ba6750fc5c051
RCdeWit <RCdeWit@users.noreply.github.com>
2023-12-01 15:00:50 +0100

    Boilerplate for 2023; solution 1.1

---
+++ 2023/day_01/solution.py
@@ -0,0 +1,55 @@
+import argparse
+import logging
+import sys
+
+def solve_1(input):
+    digits_only = []
+    for line in input:
+        digits_only.append(''.join(char for char in line if char in "0123456789"))
+    logging.debug(digits_only)
+
+    calibrations = [int(line[0]+line[-1]) for line in digits_only]
+    logging.debug(calibrations)
+
+    result = sum(calibrations)
+    return result
+
+def solve_2(input):
+    return None
+
+
+if __name__ == '__main__':
+     # Parse CLI arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-q", "--question", required=True)
+    parser.add_argument("-i", "--input", required=False)
+    args = parser.parse_args()
+
+    input_file = args.input
+    question = args.question
+
+    # Read input
+    with open(input_file) as f:
+        input = list(f.read().splitlines())
+
+    # Set up logging
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+
+    logging.info(f"Question {question} with input {input_file}")
+
+    if question == "1":
+        solution = solve_1(input)
+        logging.info(f"Found solution: {solution}")
+    elif question == "2":
+        solution = solve_2(input)
+        logging.info(f"Found solution: {solution}")
+    else:
+        logging.error("Select either question 1 or 2")
