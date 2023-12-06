81e591786752f3d379bed13c323f6bc51d20dde6
RCdeWit <RCdeWit@users.noreply.github.com>
2023-12-06 13:55:29 +0100

    Solution 6.2

--- 2023/day_06/solution.py
+++ 2023/day_06/solution.py
@@ -9,6 +9,11 @@ def parse_input(input: list):
     for i, t in enumerate(time):
         yield (t, distance[i])
 
+def parse_input_2(input: list):
+    time = int(input[0].split(":")[1].replace(" ", ""))
+    distance = int(input[1].split(":")[1].replace(" ", ""))
+    return (time, distance)
+
 def calculate_distance(total_time: int, button_held: int):
     travel_time = total_time - button_held
     distance = travel_time * button_held
@@ -23,6 +28,28 @@ def calculate_ways_to_win(total_time: int, target: int):
     
     return ways_to_win
 
+def calculate_boundary(total_time: int, target: int, search_start: int, direction: str):
+    if direction == "left":
+        boundary = int(search_start / 2)
+    else:
+        boundary = int(search_start + (search_start / 2))
+
+    if calculate_distance(total_time, boundary) > target:
+        return calculate_boundary(total_time, target, boundary, direction)
+    else:
+        if direction == "left":
+            for i in range(boundary, search_start):
+                # logging.debug(f"Try: {i}")
+                if calculate_distance(total_time, i) > target:
+                    # logging.debug(f"Found {direction} boundary: {i}")
+                    return i
+        else:
+            for i in range(search_start, boundary):
+                # logging.debug(f"Try: {i}")
+                if calculate_distance(total_time, i) < target:
+                    # logging.debug(f"Found {direction} boundary: {i-1}")
+                    return i - 1
+
 
 def solve_1(input):
     races = list(parse_input(input))
@@ -33,7 +60,10 @@ def solve_1(input):
     return result
 
 def solve_2(input):
-    return
+    race = parse_input_2(input)
+    left_boundary = calculate_boundary(race[0], race[1], race[0] / 2, "left")
+    right_boundary = calculate_boundary(race[0], race[1], race[0] / 2, "right")
+    return right_boundary - left_boundary + 1
 
 if __name__ == '__main__':
      # Parse CLI arguments
