3b01f10fb0110a979cb31238b21b4223be2f57eb
RCdeWit <RCdeWit@users.noreply.github.com>
2023-12-06 16:34:19 +0100

    Solution 3.2 a.k.a. I hate this

--- 2023/day_03/solution.py
+++ 2023/day_03/solution.py
@@ -51,8 +51,6 @@ def mapped_engine_adjacencies(input_map: list):
     
     return mapped_engine_adjacencies
 
-
-
 def solve_1(input: list):
     adjacencies = mapped_engine_adjacencies(input)
 
@@ -75,10 +73,107 @@ def solve_1(input: list):
 
     return sum(result)
 
+def find_gears(input_map: list):
+    for y, line in enumerate(input_map):
+        for x, char in enumerate(line):
+            if char == "*":
+                yield (x, y)
+
+def find_adjacent_parts_for_gear_old(input_map: list, coordinate: tuple):
+    padded_map = pad_map(input_map)
+    gear_x = coordinate[0] + 1
+    gear_y = coordinate[1] + 1
+
+    # logging.debug(["Gear",gear_x, gear_y])
+
+    for y in range(gear_y - 1, gear_y + 2):
+        x_offset = 0
+        for x in range(gear_x - 1, gear_x + 2):
+            if x_offset > 0:
+                x_offset -= 1
+                continue
+            elif padded_map[y][x].isdigit():
+                machine_part = padded_map[y][x]
+                x_offset = 1
+                left_end = right_end = False
+                while x - x_offset > 0 and x + x_offset < len(padded_map[0])-1:
+                    if padded_map[y][x-x_offset].isdigit() and not left_end:
+                        machine_part = padded_map[y][x-x_offset] + machine_part
+                    else:
+                        left_end = True
+                    if padded_map[y][x+x_offset].isdigit() and not right_end:
+                        machine_part = machine_part + padded_map[y][x+x_offset]
+                    else:
+                        right_end = True
+                    x_offset += 1
+                yield machine_part
+
+def find_adjacent_parts_for_gear(input_map: list, coordinate: tuple):
+    padded_map = pad_map(input_map)
+    gear_x = coordinate[0] + 1
+    gear_y = coordinate[1] + 1
+
+    rows = []
+    rows.append(padded_map[gear_y-1][gear_x-3:gear_x+4])
+    rows.append(padded_map[gear_y][gear_x-3:gear_x+4])
+    rows.append(padded_map[gear_y+1][gear_x-3:gear_x+4])
+
+    parsed = []
+
+    for row in rows:
+        parsed_row = ""
+        for i, char in enumerate(row):
+            if i == 0:
+                if row[1].isnumeric() and row[2].isnumeric():
+                    parsed_row = parsed_row + char
+                else:
+                    parsed_row = parsed_row + "."
+            elif i == 1:
+                if row[2].isnumeric():
+                    parsed_row = parsed_row + char
+                else:
+                    parsed_row = parsed_row + "."
+            elif i in [2, 3, 4]:
+                parsed_row = parsed_row + char
+            elif i == 5:
+                if row[4].isnumeric():
+                    parsed_row = parsed_row + char
+                else:
+                    parsed_row = parsed_row + "."
+            elif i == 6:
+                if row[4].isnumeric() and row[5].isnumeric():
+                    parsed_row = parsed_row + char
+                else:
+                    parsed_row = parsed_row + "."
+
+        parsed.append(parsed_row)
+        # 0.230..
+        # ...*...
+        # 6...101
+
+    for line in parsed:
+        part = ""
+        for char in line:
+            if char.isdigit():
+                part += char
+            elif len(part) > 0:
+                yield int(part)
+                part = ""
+
+        if len(part) > 0:
+            yield int(part)
 
 def solve_2(input: list):
-    return
+    gears = list(find_gears(input))
 
+    result = 0
+    for gear in gears:
+        parts = list(find_adjacent_parts_for_gear(input, gear))
+        logging.debug(f"Gear on line {gear[1]+1} at pos {gear[0]+1} has {parts}")
+        if len(parts) == 2:
+            result += (parts[0] * parts[1])
+
+    return result
 
 if __name__ == '__main__':
      # Parse CLI arguments
