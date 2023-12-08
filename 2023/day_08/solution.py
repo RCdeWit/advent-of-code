6e550d72e1872f0b1036636de5d08481b63b6f04
RCdeWit <RCdeWit@users.noreply.github.com>
2023-12-08 12:42:04 +0100

    Solution 8.1

---
+++ 2023/day_08/solution.py
@@ -0,0 +1,75 @@
+import argparse
+import logging
+import sys
+
+def parse_input(input: list):
+    directions = input[0]
+
+    nodes = {}
+
+    for line in input[2:]:
+        line = "".join([char for char in line if char not in " ()"])
+        start, destinations = line.split("=")
+        left, right = destinations.split(",")
+
+        nodes[start] = (left, right)
+
+    return directions, nodes
+
+def traverse_nodes(nodes: dict, start: str, directions: str):
+    i = 0
+    while start != "ZZZ":
+        direction = directions[i % len(directions)]
+        if direction == "L":
+            destination = nodes[start][0]
+        elif direction == "R":
+            destination = nodes[start][1]
+        start = destination
+        i += 1
+
+    return i
+
+
+def solve_1(input):
+    directions, nodes = parse_input(input)
+    solution = traverse_nodes(nodes, "AAA", directions)
+    return solution
+
+def solve_2(input):
+    return
+
+if __name__ == '__main__':
+     # Parse CLI arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-q", "--question", required=True)
+    parser.add_argument("-i", "--input", required=False, default='input.txt')
+    args = parser.parse_args()
+
+    input_file = args.input
+    question = args.question
+
+    # Read input
+    with open(input_file) as f:
+        input = list(f.read().splitlines())
+
+    # Set up logging
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+
+    logging.info(f"Question {question} with input {input_file}")
+
+    if question == "1":
+        solution = solve_1(input)
+        logging.info(f"Found solution: {solution}")
+    elif question == "2":
+        solution = solve_2(input)
+        logging.info(f"Found solution: {solution}")
+    else:
+        logging.error("Select either question 1 or 2")
