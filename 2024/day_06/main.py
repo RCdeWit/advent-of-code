969fde7e8a5a6737d0e572420eab4d6dcbaf64fb
RCdeWit <RCdeWit@users.noreply.github.com>
2024-12-06 15:12:59 +0100

    Small optimization in 6.2

--- 2024/day_06/main.py
+++ 2024/day_06/main.py
@@ -1,6 +1,10 @@
 import argparse
 import logging
 import sys
+import time
+
+from collections import defaultdict
+from copy import deepcopy
 
 def parse_input(input: list):
     grid = []
@@ -34,11 +38,11 @@ def move_guard(grid: list, position: tuple) -> (list, tuple):
     direction = grid[y][x]
 
     if check_out_of_bounds(grid, position):
-        logging.debug(f"Guard walks out of bounds (x={x}, y={y}, dir={direction})")
+        # logging.debug(f"Guard walks out of bounds (x={x}, y={y}, dir={direction})")
         grid[y][x] = "."
         return grid, (None, None)
 
-    logging.debug(f"Guard is at x={x}, y={y}, dir={direction}")
+    # logging.debug(f"Guard is at x={x}, y={y}, dir={direction}")
     
     if direction == "U":
         if grid[y-1][x] == "#":
@@ -77,6 +81,69 @@ def move_guard(grid: list, position: tuple) -> (list, tuple):
             grid[y][x-1] = direction
             return grid, (x-1, y)
 
+# def check_if_obstacle_works(grid: list, position: tuple, visited: dict) -> bool:
+#     x, y = position
+#     direction = grid[y][x]
+
+#     logging.debug(f"Going {direction} at x={x}, y={y}")
+
+#     look_ahead_x = 0
+#     look_ahead_y = 0
+#     while look_ahead_x + x >= 0 and look_ahead_x + x < len(grid[0]) and look_ahead_y + y >= 0 and look_ahead_y + y < len(grid):
+#         if direction == "L":
+#             look_ahead_y -= 1
+#             if "U" in visited[(x, y + look_ahead_y)]:
+#                 logging.debug(f"Going {direction} at x={x}, y={y}: obstacle would cause loop at {(x-1, y)}")
+#                 return True, (x-1, y)
+#         elif direction == "R":
+#             look_ahead_y += 1
+#             if "D" in visited[(x, y + look_ahead_y)]:
+#                 logging.debug(f"Going {direction} at x={x}, y={y}: obstacle would cause loop at {(x+1, y)}")
+#                 return True, (x+1, y)
+#         elif direction == "U":
+#             look_ahead_x += 1
+#             if "R" in visited[(x + look_ahead_x, y)]:
+#                 logging.debug(f"Going {direction} at x={x}, y={y}: obstacle would cause loop at {(x, y-1)}")
+#                 return True, (x, y-1)
+#         elif direction == "D":
+#             look_ahead_x -= 1
+#             if "L" in visited[(x + look_ahead_x, y)]:
+#                 logging.debug(f"Going {direction} at x={x}, y={y}: obstacle would cause loop at {(x, y+1)}")
+#                 return True, (x, y+1)
+
+#     return False, (None, None)
+
+def place_obstacle(grid: list, location: tuple) -> list:
+    x, y = location
+    grid[y][x] = "#"
+    return grid
+
+def detect_loop(location: tuple, direction: str, visited: dict) -> bool:
+    x, y = location
+    # logging.debug(f"Checking x={x}, y={y}, dir={direction}")
+    if direction in visited[location]:
+        # logging.debug(f"Found loop: x={x}, y={y}, dir={direction}")
+        return True
+    return False
+
+def count_guard_steps(grid: list, position: tuple) -> (int, dict):
+    out_of_bounds = False
+    steps = 0
+    visited = defaultdict(set)
+
+    while not out_of_bounds:
+        x, y = position
+        direction = grid[y][x]
+        visited[position].add(direction)
+        grid, position = move_guard(grid, position)
+        steps += 1
+        if detect_loop(position, grid[y][x], visited):
+            return -1, visited
+        elif position[0] is None:
+            out_of_bounds = True
+
+    return len(visited)-1, visited
+
 def solve_1(input: list) -> int:
     grid, position = parse_input(input)
 
@@ -95,9 +162,27 @@ def solve_1(input: list) -> int:
 
     return len(visited)
 
-
 def solve_2(input: list) -> int:
-    pass
+    grid, position = parse_input(input)
+    viable_obstacles = {}
+
+    original_path = count_guard_steps(deepcopy(grid), position)[1]
+
+    for potential_obstacle in original_path.keys():
+        x, y = potential_obstacle
+        modified_grid = deepcopy(grid)
+        if potential_obstacle == position or x is None or y is None:
+            pass
+        else:
+            # logging.debug(f"Trying obstacle at x={x} y={y}")
+            modified_grid[y][x] = "#"
+
+            if count_guard_steps(modified_grid, position)[0] == -1:
+                # logging.debug(f"Found obstacle at x={x} y={y}")
+                viable_obstacles[(x, y)] = True
+
+    return len(viable_obstacles)
+
 
 if __name__ == '__main__':
      # Parse CLI arguments
@@ -125,12 +210,13 @@ if __name__ == '__main__':
     root.addHandler(handler)
 
     logging.info(f"Question {question} with input {input_file}")
+    start = time.time()
 
     if question == "1":
         solution = solve_1(input)
-        logging.info(f"Found solution: {solution}")
+        logging.info(f"Found solution: {solution} (in {time.time()-start} seconds)")
     elif question == "2":
         solution = solve_2(input)
-        logging.info(f"Found solution: {solution}")
+        logging.info(f"Found solution: {solution} (in {time.time()-start} seconds)")
     else:
         logging.error("Select either question 1 or 2")
